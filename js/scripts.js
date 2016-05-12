eeef2a7a56757ca5d08c70f93b80ba0fb231d33d
Shelby Spiering <sspiering@eagles.ewu.edu>
2016-05-12 14:30:38 -0700

    Whoop! I got it!

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
 // My Scripts
 $('article').readmore({
-  speed: 75,
-  lessLink: '<a href="#">Read less</a>'
+  speed: 1000,
+  lessLink: '<a href="#">&#x1F60D</a>',
+  moreLink: '<a href="#">&#x1F602;</a>'
 });
